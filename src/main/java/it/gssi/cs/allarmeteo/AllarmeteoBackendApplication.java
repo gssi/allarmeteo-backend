d5c490f640bdd6b7a0449dcd70fac1121aafd5a2
claudiodyd <claudio.pompilio@gmail.com>
2024-01-26 14:13:53 +0100

    Add Spring Boot initial project

---
+++ src/main/java/it/gssi/cs/allarmeteo/AllarmeteoBackendApplication.java
@@ -0,0 +1,13 @@
+package it.gssi.cs.allarmeteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllarmeteoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllarmeteoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gssi/cs/allarmeteo/AllarmeteoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gssi.cs.allarmeteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllarmeteoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
