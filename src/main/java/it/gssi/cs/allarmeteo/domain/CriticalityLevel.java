506034b34392dc9e37da6aebdf342ce501521402
claudiodyd <claudio.pompilio@gmail.com>
2024-02-21 10:21:26 +0100

    Optimize import CriticalityLevel entity

--- src/main/java/it/gssi/cs/allarmeteo/domain/CriticalityLevel.java
+++ src/main/java/it/gssi/cs/allarmeteo/domain/CriticalityLevel.java
@@ -1,18 +1,13 @@
 package it.gssi.cs.allarmeteo.domain;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Entity
 @Table(name = "criticality_levels")
